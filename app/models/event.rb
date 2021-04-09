620267b063070fc2f0a3cf293772a257ee478a02
FdI96 <federicolopezce@gmail.com>
2021-04-09 18:05:34 -0300

    Adding many to many association between users, events and a through table attendances with the corresponding names

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,8 @@
 class Event < ApplicationRecord
+  #Event is created by a single user (creator)
   belongs_to :creator, class_name: 'User'
+
+  #Events has atttendees
+  has_many :attendances, foreign_key: :attended_event_id
+  has_many :attendees, through: :attendances
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_09_150947) do
+ActiveRecord::Schema.define(version: 2021_04_09_203832) do
 
   create_table "attendances", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "attendee_id"
+    t.integer "attended_event_id"
   end
 
   create_table "events", force: :cascade do |t|
@@ -24,14 +26,16 @@ ActiveRecord::Schema.define(version: 2021_04_09_150947) do
     t.integer "creator_id"
   end
 
-  create_table "users", force: :cascade do |t|   
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name"  
+    t.string "name"
+    t.index ["email"], name: "index_users_on_email"
     t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
