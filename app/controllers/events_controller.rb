1b5948da90f0657b99a18b36392d2540ea0ae295
FdI96 <federicolopezce@gmail.com>
2021-04-13 11:58:26 -0300

    Add past and upcoming on index action in Event controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,7 +3,8 @@ class EventsController < ApplicationController
 
   # GET /events or /events.json
   def index
-    @events = Event.all
+    @past = Event.past
+    @upcoming = Event.upcoming
   end
 
   # GET /events/1 or /events/1.json
