b618ea84b4995da1127e58065d40875f2252923d
FdI96 <federicolopezce@gmail.com>
2021-04-12 23:21:46 -0300

    Use build method on create action on events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -30,7 +30,7 @@ class EventsController < ApplicationController
 
   # POST /events or /events.json
   def create
-    @event = Event.new(event_params)
+    @event = current_user.events_created.build(event_params)
 
     respond_to do |format|
       if @event.save
